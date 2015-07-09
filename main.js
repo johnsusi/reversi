3ad695dbe77c1ef72f0763555584bf4d7812b534
John Susi <john@susi.se>
2015-07-09 17:36:39 +0200

    Add initial test and fix some paths in the native build

---
+++ main.js
@@ -0,0 +1,13 @@
+var path = require('path');
+
+var engine = 'node';
+if (process.versions['electron']) engine = 'electron';
+if (process.versions['nwjs'])     engine = 'nwjs';
+
+var version = process.versions[engine];
+
+var name = ['reversi', process.platform, process.arch, engine, version].join('-');
+
+var full_path = path.join(__dirname, 'build', name);
+
+module.exports = require(full_path);
---
+++ test/index.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var reversi = require('..');
+
+describe('reversi native addon', function() {
+
+  it('must be defined', function () {
+    assert(reversi);
+  });
+
+});
