8e7c76a0842a32e22e4917a5c81397bcc3fb39e0
John Susi <john@susi.se>
2015-07-10 22:35:12 +0200

    Add bindings addon

--- main.js
+++ main.js
@@ -8,14 +8,6 @@ var version = process.versions[engine];
 
 var name = ['reversi', process.platform, process.arch, engine, version].join('-');
 
-var full_path = path.join(__dirname, 'build', name);
+//var full_path = path.join(__dirname, 'build', name);
 
-try {
-  module.exports = require(full_path);
-}
-catch(e) {
-  full_path = path.join(__dirname, 'build', 'Release', name);
-  module.exports = require(full_path);
-}
-
-console.log(full_path);
+module.exports = require('bindings')(name);
